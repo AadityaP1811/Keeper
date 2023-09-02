2ceeac3eae35fa91ec973c4deeffd6ffbf89c7d3
Aaditya Parmar <94757097+AadityaP1811@users.noreply.github.com>
2023-09-02 16:59:06 +0530

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Header() {
+  return (
+    <header>
+      <h1>Keeper <img  src="icon.png" class = "immg" alt="img" height="50px" /></h1>
+    </header>
+  );
+}
+
+export default Header;
